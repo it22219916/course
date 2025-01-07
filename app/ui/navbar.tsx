61bc42dd603b0ff8f99603ee899fa2cf85cc4db4
Harshamal Vishwajith <it22219916@my.sliit.lk>
2025-01-07 23:24:23 +0530

    navbar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -38,7 +38,7 @@ const Navbar: React.FC = () => {
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link href="https://flowbite.com" className="flex items-center">
+          <Link href="/" className="flex items-center">
             <Image
               src={Logo}
               className="mr-3 h-6 w-auto sm:h-9"
